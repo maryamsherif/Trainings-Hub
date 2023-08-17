d0450e7eb3809bbe9e51bb2ed5d5608784ebb5b8
Amera Salah <54145386+amerasalah7@users.noreply.github.com>
2023-08-17 14:52:30 +0300

    springboot project + generate contract

---
+++ src/main/java/com/example/trainingHub/TrainingHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainingHub/TrainingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
